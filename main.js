50f927dd3080a844dae1dff88b821bae24690031
rubikloc <loic.charpentier@gmail.com>
2015-02-15 14:25:31 +0100

    msg erreur login + refacto des services

--- main.js
+++ main.js
@@ -9,52 +9,31 @@
 	        otherwise({redirectTo: '/signup'});
     }]);
 
-	app.run(function(){
+	app.run(['$rootScope', function($rootScope){
 		Parse.initialize("HCf9xf3DDDBH4BTY3qwRqxVHeiQ2GW1V2JIx6KBv", "78VAu3yojn4H5bhjKEPa1opxy3bHmQSfPk8S1dAo");
-	});
+    	$rootScope.sessionUser = Parse.User.current();
+	}]);
 
-	app.service('sessionService', ['$rootScope','$q', '$location', function($rootScope, $q, $location){
+	app.service('sessionService', ['$q', function($q){
 
 		this.login = function(user){
-
 			var loginDefered = $q.defer();
-			var loginPromise = loginDefered.promise;
-
-			loginPromise.then(
-				function(user){	
-					$rootScope.sessionUser = user;
-					$location.path('/home');
-				//	console.log($rootScope.sessionUser.get("username"));
-				},
-				function(error){
-					console.log(error);
-				});
 
 			Parse.User.logIn(user.email,user.password, {
 				success: function(user){
 					loginDefered.resolve(user);
 				},
-				error:function(error){
-					loginDefered.reject(error);
+				error:function(user,errorMsg){
+					loginDefered.reject(errorMsg);
 				}
 			});
+			return loginDefered.promise;
 
 		};
 		
 		this.signup = function(user) {
 			var signupDefered = $q.defer();
-        	var signupPromise = signupDefered.promise;
         
-	        signupDefered.promise
-	            .then(function(user){
-						console.log("utilisateur créé ",user.objectId);
-	                    $rootScope.sessionUser = user;
-	                    $location.path('/home');
-	                },
-	                function(msg,object){
-						console.log(msg);	                    
-	                });
-	        
 	        var newUser = new Parse.User();
     		newUser.set("username",user.email);
     		newUser.set("email",user.email);
@@ -64,73 +43,89 @@
     				success : function(user){
     				    signupDefered.resolve(user);
     					},
-    				error : function(object,msg){
-    				    signupDefered.reject(msg,object);
+    				error : function(user,errorMsg){
+    				    signupDefered.reject(errorMsg);
     				}
     		});
+    		return signupDefered.promise;
+
 		};
 
 		this.logout = function(){
 			Parse.User.logOut();
-			$location.path('/signin');
 		};
-
-
 	}]);
 
 	app.factory('applicationFactory', ['$q', function($q){
 		
-		this.getAppFiles = function(user){
+		return {
+			getAppFiles: function(user){
 
-			var appFilesDefered = $q.defer();
-	        var appFilesPromise = appFilesDefered.promise;
+				var appFilesDefered = $q.defer();
 
-			var JobApplication = Parse.Object.extend("JobApplication");        
-	        var appFilesQuery = new Parse.Query(JobApplication);
-	        appFilesQuery.equalTo("parent",user);
-	        
-	        appFilesQuery.find({
-	           success : function(results){
-	               appFilesDefered.resolve(results);
-	           },
-	           error : function(object, error){
-	               appFilesDefered.reject(object,error);
-	           }
-	        });
-	        return appFilesPromise;
+				var JobApplication = Parse.Object.extend("JobApplication");        
+		        var appFilesQuery = new Parse.Query(JobApplication);
+		        appFilesQuery.equalTo("parent",user);
+		        
+		        appFilesQuery.find({
+		           success : function(results){
+		               appFilesDefered.resolve(results);
+		           },
+		           error : function(object, error){
+		               appFilesDefered.reject(object,error);
+		           }
+		        });
+		        return appFilesDefered.promise;
+		    }
         };
-		return this;
+	}])
+
+	app.factory('uploadFactory', ['$q', function($q){
+		return function upload(files){
+			
+		};
 	}])
 
 
-    app.controller('signupCtrl', ['sessionService','$scope', function(sessionService,$scope){
+    app.controller('signupCtrl', ['sessionService','$scope','$rootScope','$location', function(sessionService,$scope,$rootScope,$location){
     	this.signUp = function() {
-    		sessionService.signup($scope.user);
+    		sessionService.signup($scope.user).
+    			then(
+	    			function(user){
+	                    $rootScope.sessionUser = user;
+	                    $location.path('/home');
+	                },
+	                function(errorMsg){
+	                	$scope.signError = errorMsg.message;           
+	                });				
     	};
     }]);
 
 
-    app.controller('signinCtrl', ['sessionService','$scope', function(sessionService,$scope){
+    app.controller('signinCtrl', ['sessionService','$scope','$rootScope','$location', function(sessionService,$scope,$rootScope,$location){
     	this.signIn = function(){
-    		console.log($scope);
-    		sessionService.login($scope.user);
+    		sessionService.login($scope.user).
+    			then(
+    				function(user){
+						$rootScope.sessionUser = user;
+						$location.path('/home');
+    				},
+    				function(errorMsg){
+	                	$scope.signError = errorMsg.message;
+    				});
     	};
     }]);
 
 
-    app.controller('headerCtrl', ['sessionService', '$scope', function(sessionService, $scope){
+    app.controller('headerCtrl', ['sessionService', '$location', function(sessionService, $location){
     	this.logOut = function(){
     		sessionService.logout();
+			$location.path('/signin');
     	};
     }])
 
     app.controller('homeCtrl', ['$rootScope','$scope', 'applicationFactory', '$q', '$route', function($rootScope,$scope,applicationFactory,$q, $route){
     	
-
-// Problem to solve
-    	$rootScope.sessionUser = Parse.User.current();
-// Problem to solve
-
     	applicationFactory.getAppFiles($rootScope.sessionUser).
             then(function(results){
         		var urls =[];
